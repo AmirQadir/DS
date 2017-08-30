28c37ba8023b4a83e97fe29a8a4e7e20fa7e3c06
AmirAli <k163641@nu.edu.pk>
2017-08-30 21:38:41 +0500

    LinkedList
    Singly and DoublyLinked List

---
+++ DoubleLinkedList.java
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+<nppcrypt version="1012">
+<encryption cipher="rijndael256" mode="gcm" encoding="base64" tag="x5hDuMcvax6QDJTnTmeJ0Q==" />
+<random iv="LnloMtTuKuq6FPwZyLXSrQ==" salt="f/EVoSARJv15/9CGqprP+w==" />
+<key algorithm="scrypt" N="16384" r="8" p="1" />
+</nppcrypt>
+VEpc2MrYTG/AoEwuD3q8xdMZJ1rwe3nRlAomt+OBfPhMD54=
